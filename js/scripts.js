373354d9f0016d81410b82a581a10a2e040937f3
Guest User <Guest@Epicodus-7E.local>
2018-01-23 11:44:23 -0800

    have html layout, Movie object generator, and js form logic Mk1 complete.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,65 @@
-$(function(){
+function Movie(name, rating, adult, child, senior) {
+  this.name = name;
+  this.rating = rating;
+  this.adult = {
+    "12:00": 9,
+    "1:30" : 9,
+    "3:45": 9,
+    "5:00": 11,
+    "7:30": 11,
+    "10:00": 11
+  };
+  this.child = {
+    "12:00": 6,
+    "1:30": 6,
+    "3:45": 6,
+    "5:00": 9,
+    "7:30": 9,
+    "10:00": 9
+  };
+  this.seinor = {
+    "12:00": 6.5,
+    "1:30": 6.5,
+    "3:45": 6.5,
+    "5:00": 10,
+    "7:30": 10,
+    "10:00": 10
+  };
+}
+house = []
+
+function ticketPrice(Movie, time, tier) {
+
+}
 
 
 
+$(function() {
 
+  $("#movie-form").change(function(event) {
+    movieChoice = $("input:radio[name=movie]:checked").val();
+    event.preventDefault();
+    $("#which-time").delay(950).fadeTo(500, 1);
+    console.log(movieChoice);
+
+  });
+  $("#time-form").change(function(event) {
+    timeChoice = $("input:radio[name=time]:checked").val();
+    event.preventDefault();
+    $("#which-tier").delay(950).fadeTo(500, 1);
+    console.log(timeChoice);
+
+  });
+  $("#tier-form").change(function(event) {
+    tierChoice = $("input:radio[name=tier]:checked").val();
+    event.preventDefault();
+    $("#which-time").delay(950).fadeTo(500, 1);
+    console.log(tierChoice);
+
+  });
+var billboards = new Movie("Three Billboards Outside Ebbing", "R")
+var money = new Movie("All the Money in the World", "R")
+var tonya = new Movie("I Tonya", "R")
+house.push(tonya, money, billboards);
+console.log(house);
 });
