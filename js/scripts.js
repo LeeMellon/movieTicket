1e3f9f0ef4daa2c9ba77e411138393494e6dc4fa
Guest User <Guest@Epicodus-7E.local>
2018-01-23 16:29:53 -0800

    remove which movie box and made the movie selector the poster images. Add mmouse over film synopsis.

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,7 @@ function Movie(name, rating, adult, child, senior) {
   this.rating = rating;
   this.adult = {
     "12:00": 9.00,
-    "1:30" : 9.00,
+    "1:30": 9.00,
     "3:45": 9.00,
     "5:00": 11.00,
     "7:30": 11.00,
@@ -28,14 +28,14 @@ function Movie(name, rating, adult, child, senior) {
 }
 house = []
 
-function ticketPrice(){
-  for(index = 0; index < house.length; index ++){
-    if (movieChoice === house[index].name){
-    myMovie = house[index];
-    var myPrice = myMovie[tierChoice][timeChoice];
-    var priceEl = document.getElementById("price");
-    priceEl.innerHTML = "$ " + myPrice;
-    return myPrice
+function ticketPrice() {
+  for (index = 0; index < house.length; index++) {
+    if (movieChoice === house[index].name) {
+      myMovie = house[index];
+      var myPrice = myMovie[tierChoice][timeChoice];
+      var priceEl = document.getElementById("price");
+      priceEl.innerHTML = "$ " + myPrice;
+      return myPrice
     }
   }
 }
@@ -46,33 +46,39 @@ function ticketPrice(){
 
 $(function() {
 
-  $("#movie-form").change(function(event) {
-    movieChoice = $("input:radio[name=movie]:checked").val();
-    $("#which-time").delay(450).fadeTo(500, 1);
+$(".description1").click(function(event) {
+  movieChoice = "I Tonya"
+  $("#which-time").delay(450).fadeTo(500, 1);
 
-  });
-  $("#time-form").change(function(event) {
-    timeChoice = $("input:radio[name=time]:checked").val();
-    $("#which-tier").delay(450).fadeTo(500, 1);
+});
+$(".description2").click(function(event) {
+  movieChoice = "Three Billboards Outside Ebbing"
+  $("#which-time").delay(450).fadeTo(500, 1);
+});
 
-  });
-  $("#tier-form").change(function(event) {
-    tierChoice = $("input:radio[name=tier]:checked").val();
-    ticketPrice();
-    // var priceEl = document.getElementById("price");
-    // priceEl = "$ " + myPrice;
-    $(".form-container").delay(100).fadeOut(700, "swing", "complete")
-    $(".price-display").delay(850).fadeIn(900, "swing", "complete");
+$(".description3").click(function(event) {
+  movieChoice = "All the Money in the World"
+  $("#which-time").delay(450).fadeTo(500, 1);
+});
 
-  });
+$("#time-form").change(function(event) {
+  timeChoice = $("input:radio[name=time]:checked").val();
+  $("#which-tier").delay(450).fadeTo(500, 1);
+
+});
+$("#tier-form").change(function(event) {
+  tierChoice = $("input:radio[name=tier]:checked").val();
+  ticketPrice();
+  $(".form-container").delay(100).fadeOut(700, "swing", "complete")
+  $(".price-display").delay(850).fadeIn(900, "swing", "complete");
+
+});
 
 
 
-
-
-var billboards = new Movie("Three Billboards Outside Ebbing", "R")
-var money = new Movie("All the Money in the World", "R")
-var tonya = new Movie("I Tonya", "R")
-house.push(tonya, money, billboards);
-console.log(house);
+  var billboards = new Movie("Three Billboards Outside Ebbing", "R")
+  var money = new Movie("All the Money in the World", "R")
+  var tonya = new Movie("I Tonya", "R")
+  house.push(tonya, money, billboards);
+  console.log(house);
 });
